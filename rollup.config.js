70585ecea26cdfa1671ad54d5930dc04ae986ac2
João Vitor Moreira <themoreira.jvm@gmail.com>
2023-02-24 19:00:14 -0300

    build: add Rollup with build npm script

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'umd',
+    name: 'reatomic',
+    sourcemap: true,
+    globals: {
+      'react': 'React',
+      'styled-components': 'styled',
+    },
+  },
+  external: ['react', 'react-dom', 'styled-components'],
+  plugins: [typescript({
+    outputToFilesystem: false,
+  })],
+};
