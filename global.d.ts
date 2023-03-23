563a92a127a21fb6eba29a80eda72d41305f07c2
João Vitor Moreira <themoreira.jvm@gmail.com>
2023-03-22 22:06:33 -0300

    feat(globals): introduce AllOrNothing and OneOrAnother generic types

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,11 @@
 import 'jest-extended';
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+
+declare global {
+  type AllOrNothing<T extends Record<string, unknown>> = T | Partial<Record<keyof T, never>>;
+
+  type OneOrAnother<One, Another> =
+    | One & Partial<Record<keyof Another, never>>
+    | Another & Partial<Record<keyof One, never>>;
+}
